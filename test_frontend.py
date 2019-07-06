162f4ab65e32c0e638d4d659ceed11cb5810b051
Brian Torpey <brt52@pitt.edu>
2019-07-06 17:08:37 -0400

    Added fix for new category

--- test_frontend.py
+++ test_frontend.py
@@ -1,23 +1,122 @@
 from flask import Flask, request, session, render_template, abort, redirect, url_for, flash, make_response
+from flask_restful import reqparse, abort, Api, Resource, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-from models import db, User, Account
+from models import db, User, Account, Transaction
+from datetime import datetime
 import os
+import re
 
 app = Flask(__name__)
+api = Api(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    app.root_path, "main.db"
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/cashify_dev"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
+'''*****RESTful Resources*****'''
+parser = reqparse.RequestParser()
+parser.add_argument('Amount')
+parser.add_argument('Category')
+
+
+# Game Resource
+# Creates Games and Deletes Games
+class MyTransaction(Resource):
+    def delete(self, transaction_id):
+        Transaction.query.filter_by(id=transaction_id).delete()
+        db.session.commit()
+        return '', 204
+
+
+# TransactionList
+# shows a list of all transactions, and lets you POST to add new transactions
+class TransactionList(Resource):
+    def get(self):
+        username = session["username"]
+
+        user = User.query.get(1)
+        for u in User.query.all():
+            if u.username == username:
+                user = u
+
+        account = user.account
+        transactions = account.transactions
+
+        if (len(transactions) != 0):
+            transactionList = []
+            # transactionList will include the information of each transaction. Amount, date, and category. The last object is the user's account at that moment.
+            for t in transactions:
+                transactionList.append(t.date)
+                transactionList.append(t.amount)
+                transactionList.append(t.category)
+                transactionList.append(t.current_balance)
+            return transactionList
+        else:
+            return None
+
+    def post(self):
+        args = parser.parse_args()
+        amount = args['Amount']
+        category = args['Category']
+
+        # Get the user
+        username = session["username"]
+
+        user = User.query.get(1)
+        for u in User.query.all():
+            if u.username == username:
+                user = u
+
+        account = user.account
+        transactions = account.transactions
+        tempBalance = account.balance + float(amount)
+        account.balance = round(tempBalance, 2)
+        balance = account.balance
+
+        dateX = datetime.now()
+        date = myconverter(dateX)
+
+        # Create the Transaction
+        t = Transaction(amount=amount, date=date, category=category, current_balance=balance)
+        db.session.add(t)
+        account.transactions.append(t)
+        db.session.commit()
+
+        return t.id, 201
+
+
+def myconverter(o):
+    if isinstance(o, datetime):
+        # Formatting
+        if (o.month < 10):
+            month = "0" + str(o.month)
+        else:
+            month = o.month
+        if (o.day < 10):
+            day = "0" + str(o.day)
+        else:
+            day = o.day
+
+        return "{}-{}-{}".format(o.year, month, day)
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TransactionList, '/transactions')
+api.add_resource(MyTransaction, '/transactions/<transaction_id>')
+
+'''*****Webpage Functions*****'''
+
 
 # by default, direct to login
 @app.route("/")
 def default():
     return redirect(url_for("home"))
 
-#Login Function     
+
+# Login Function
 @app.route("/home/", methods=["GET", "POST"])
 def home():
     # first check if the user is already logged in
@@ -26,7 +125,7 @@ def home():
         for u in User.query.all():
             if u.username is session["username"]:
                 user = u
-    
+
         return render_template("userpage.html", user=user)
 
     # if not, and the incoming request is via POST try to log them in
@@ -39,7 +138,7 @@ def home():
             for u in User.query.all():
                 if u.username is thisUsername:
                     user = u
-            print(user.password_hash)
+
             if thisPassword == user.password_hash:
                 session["username"] = thisUsername
                 return render_template("userpage.html", user=user)
@@ -50,6 +149,7 @@ def home():
     else:
         return render_template("homepage.html")
 
+
 @app.route("/logout/")
 def unlogger():
     # if logged in, log out, otherwise offer to log in
@@ -59,20 +159,24 @@ def unlogger():
     else:
         return redirect(url_for("home"))
 
+
 # Redirects user to registration page with their information.
 @app.route("/registration/", methods=["POST"])
 def regRedirect():
     rUsername = request.form["rusername"]
     rPassword = request.form["rpassword"]
-    cPassword = request.form["cpassword"]   
-    if(rUsername != "" and rPassword != "" and cPassword != ""):
+    cPassword = request.form["cpassword"]
+    if (rUsername != "" and rPassword != "" and cPassword != ""):
         return render_template("registration.html", rusername=rUsername, rpassword=rPassword, cpassword=cPassword)
     else:
         return redirect(url_for("home"))
 
+
+'''
 @app.route("/registrationCheck/", method="POST")
 def registration():
-    return render_template("registration.html")
+    return render_template("registration.html")'''
+
 
 # CLI Commands
 @app.cli.command("initdb")
@@ -81,21 +185,27 @@ def init_db():
     db.drop_all()
     db.create_all()
 
-    print("Initialized Connect 4 Database.")
+    print("Initialized Cashify Database.")
+
 
 @app.cli.command("devinit")
 def init_dev_data():
     """Initializes database with data for development and testing"""
     db.drop_all()
     db.create_all()
-    print("Initialized Connect 4 Database.")
+    print("Initialized Cashify Database.")
 
+    a1 = Account(balance=0.00)
+    u1 = User(username="Brian", password_hash="Torpey")
 
-    u1 = User(username="Tyler", password_hash="Vogel")
-
+    db.session.add(a1)
     db.session.add(u1)
+
+    u1.account = a1
+
     db.session.commit()
     print("Added dummy data.")
+
 
 # needed to use sessions
 # note that this is a terrible secret key
